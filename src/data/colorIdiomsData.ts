faa1fc196230df9e85f8420b1066ad99ce278fd0
artisyn <artioms1@gmail.com>
2022-07-06 20:40:22 +0300

    new data

---
+++ src/data/colorIdiomsData.ts
@@ -0,0 +1,244 @@
+interface idiomObj {
+	idiom: string;
+	definition: string;
+	question: string;
+	a: string;
+	b: string;
+	c: string;
+	d: string;
+	correct: string;
+}
+
+export const idioms: idiomObj[] = [
+	{
+		idiom: 'in the red',
+		definition:
+			'If a person or organization is in the red, they owe money to someone or to another organization.',
+		question:
+			"I have so many outstanding parking tickets that I'll be in the  _____  if I pay them all off at once!",
+		a: 'blue',
+		b: 'red',
+		c: 'green',
+		d: 'black',
+		correct: 'red',
+	},
+	{
+		idiom: 'be yellow',
+		definition: 'To be cowardly.',
+		question:
+			"I'm not surprised that Tom didn't come to the rally — he’s too  _____  to defend his beliefs in public.",
+		a: 'yellow',
+		b: 'red',
+		c: 'white',
+		d: 'velvet',
+		correct: 'yellow',
+	},
+	{
+		idiom: 'cry blue murder',
+		definition: 'To scream or shout very loudly.',
+		question:
+			'Erika cried  _____  murder when she noticed the snake in the rocks next to her.',
+		a: 'orange',
+		b: 'black',
+		c: 'red',
+		d: 'blue',
+		correct: 'blue',
+	},
+	{
+		idiom: 'the black sheep of the family',
+		definition: 'The worst member of the family.',
+		question:
+			"Mary is the  _____  sheep of the family. She's always in trouble with the police.",
+		a: 'silver',
+		b: 'brown',
+		c: 'black',
+		d: 'red',
+		correct: 'black',
+	},
+	{
+		idiom: 'have green fingers',
+		definition:
+			'If someone has green fingers, they are very good at gardening.',
+		question:
+			"My mom can keep any plant alive, even ones that I've nearly killed—she seriously has  _____  fingers.",
+		a: 'green',
+		b: 'gold',
+		c: 'violet',
+		d: 'blue',
+		correct: 'green',
+	},
+	{
+		idiom: 'see pink spiders',
+		definition:
+			'To hallucinate or see things incorrectly due to acute alcohol intoxication or withdrawal.',
+		question:
+			"You're going to start seeing  _____  spiders if you don't stop drinking so much.",
+		a: 'pink',
+		b: 'white',
+		c: 'dark',
+		d: 'black',
+		correct: 'pink',
+	},
+	{
+		idiom: 'born in/to the purple',
+		definition:
+			'Born into royalty. Purple is a color traditionally associated with royalty.',
+		question:
+			'Did you see pictures of the latest addition to the royal family? What a sweet little princess born to the  _____  !',
+		a: 'yellow',
+		b: 'green',
+		c: 'purple',
+		d: 'blue',
+		correct: 'purple',
+	},
+	{
+		idiom: 'gray divorce',
+		definition:
+			'Divorce between older couples (i.e., those who have gray hair).',
+		question:
+			'Obviously, there is a trend of  _____  divorces; the question is, why do people want to split up after 30 or more years of marriage?',
+		a: 'silver',
+		b: 'gray',
+		c: 'light',
+		d: 'white',
+		correct: 'gray',
+	},
+	{
+		idiom: 'do up brown',
+		definition:
+			'To do something to the maximum degree or with the maximum effect or impact.',
+		question:
+			'Of course she decorated the room beautifully for the birthday party—she does everything up  _____  .',
+		a: 'red',
+		b: 'blue',
+		c: 'black',
+		d: 'brown',
+		correct: 'brown',
+	},
+	{
+		idiom: 'the white hope',
+		definition:
+			'The person or thing expected to bring great success or fortune to someone or something.',
+		question:
+			"Many are talking about the rising football star as the team's  _____  hope.",
+		a: 'pink',
+		b: 'orange',
+		c: 'white',
+		d: 'silver',
+		correct: 'white',
+	},
+	{
+		idiom: 'turn green with envy',
+		definition: 'To become very jealous of another person.',
+		question:
+			"My son turns  _____  with envy whenever his sister gets something and he doesn't.",
+		a: 'green',
+		b: 'black',
+		c: 'yellow',
+		d: 'brown',
+		correct: 'green',
+	},
+	{
+		idiom: 'the yellow brick road',
+		definition:
+			'A course of action that a person takes believing that it will lead to good things',
+		question:
+			'Jenna took the internship thinking that she was following the  _____  brick road to her dream job.',
+		a: 'white',
+		b: 'blue',
+		c: 'yellow',
+		d: 'gold',
+		correct: 'yellow',
+	},
+	{
+		idiom: 'a red herring',
+		definition:
+			'Something irrelevant that diverts attention away from the main problem or issue.',
+		question:
+			'The book is fast-paced, exciting, and full of  _____  herrings.',
+		a: 'red',
+		b: 'green',
+		c: 'black',
+		d: 'violet',
+		correct: 'red',
+	},
+	{
+		idiom: 'out of the blue',
+		definition: 'Completely unexpectedly',
+		question:
+			'She had no idea that anything was wrong until he announced out of the  _____  that he wanted a divorce.',
+		a: 'white',
+		b: 'black',
+		c: 'blue',
+		d: 'red',
+		correct: 'blue',
+	},
+	{
+		idiom: 'a black look',
+		definition: 'An angry or a disapproving expression on somebody’s face.',
+		question:
+			"I tried to apologize for what I said, but she gave me such a  _____  look that I knew it wasn't the right time to talk to her yet.",
+		a: 'green',
+		b: 'yellow',
+		c: 'red',
+		d: 'black',
+		correct: 'black',
+	},
+	{
+		idiom: 'a pink slip',
+		definition: 'A notice of termination from an employer',
+		question:
+			'He was in total disbelief when he received a  _____  slip from his boss today, as he had worked at the company for over a decade.',
+		a: 'pink',
+		b: 'blue',
+		c: 'silver',
+		d: 'orange',
+		correct: 'pink',
+	},
+	{
+		idiom: 'the men in gray suits',
+		definition:
+			'People working in politics, law, etc. who have power but are not known to the public.',
+		question:
+			'It will be the men in  _____  suits who decide whether the Prime Minister stays or goes.',
+		a: 'white',
+		b: 'gray',
+		c: 'red',
+		d: 'blue',
+		correct: 'gray',
+	},
+	{
+		idiom: 'red-handed',
+		definition: 'In the very act of a crime, wrongdoing, etc.',
+		question:
+			'He tried to deny that he had stolen the money, but he was caught  _____  -handed by security cameras.',
+		a: 'yellow',
+		b: 'black',
+		c: 'blue',
+		d: 'red',
+		correct: 'red',
+	},
+	{
+		idiom: 'a (little) white lie',
+		definition:
+			"A lie thought to be insignificant and justified, especially one told to avoid hurting someone's feelings or giving offense.",
+		question:
+			'I didn’t like her new haircut, but I didn’t want to upset her, so I told a little  _____  lie instead.',
+		a: 'black',
+		b: 'brown',
+		c: 'white',
+		d: 'blue',
+		correct: 'white',
+	},
+	{
+		idiom: 'the brown bottle flu',
+		definition: 'A hangover or sickness from drinking',
+		question:
+			"I'm not surprised you have the  _____  bottle flu today—do you remember last night at the bar at all?",
+		a: 'brown',
+		b: 'blue',
+		c: 'black',
+		d: 'white',
+		correct: 'brown',
+	},
+];
